4352552daa72e762dcfd8264961964cec5802774
Khasinjy <k.praxele@gmail.com>
2020-07-03 00:53:53 +0200

    GESTION CLIENT, MENUS(footer,header), textes, suppression hebergement, edition hebergement (fonctionne mais souci de presentation)

--- src/main/java/com/scholaholiday/scholaholiday/ClientController.java
+++ src/main/java/com/scholaholiday/scholaholiday/ClientController.java
@@ -2,6 +2,7 @@ package com.scholaholiday.scholaholiday;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,14 +14,15 @@ public class ClientController {
     private ClientRepository clientRepository;
 
     @GetMapping("/addclient")
-    public String addClient(Client client) {
+    public String addClient(Client client, Model model) {
+        model.addAttribute("clients",clientRepository.findAll());
         return "add-reservationClient";
     }
 
     @PostMapping("/addclient")
     public String addingClient(Client client, BindingResult result) {
         clientRepository.save(client);
-        return "redirect:/addreservation";
+        return "redirect:/addclient";
     }
 
 }
--- src/main/java/com/scholaholiday/scholaholiday/HebergementController.java
+++ src/main/java/com/scholaholiday/scholaholiday/HebergementController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
@@ -29,6 +30,26 @@ public class HebergementController {
     public String addingHebergement(Hebergement hebergement, BindingResult result) {
         hebergementRepository.save(hebergement);
         return "redirect:/addhebergement";
+    }
+
+    @GetMapping("/hebergement/{id}")
+    public String getHebergementById(@PathVariable int id, Model model) {
+        model.addAttribute("hebergement", hebergementRepository.findById(id).get());
+        return "editHebergement";
+    }
+
+    @PostMapping("/edithebergement/{id}")
+    public String editHebergement(Hebergement hebergement, @PathVariable int id) {
+        hebergement.setId(id);
+        hebergementRepository.save(hebergement);
+        return "redirect:/hebergement/"+id;
+    }
+
+    @PostMapping("/deletehebergement/{id}")
+    public String deleteHebergement(@PathVariable int id) {
+        hebergementRepository.deleteById(id);
+        return "redirect:/viewallhebergements";
+
     }
 
 }
